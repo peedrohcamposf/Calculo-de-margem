15eabc95f91c92500f0f7459a6d094a2c63242fa
Vitor Oliveira <vitorsimon04@gmail.com>
2025-12-01 20:09:33 -0300

    REMOVE - Exclusão de HTML, CSS e JS base

--- js/features/header.js
+++
@@ -1,9 +0,0 @@
-export function inicializarHeaderCorporativo() {
-  const header = document.querySelector(".header-corp");
-  if (!header) return;
-
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 10) header.classList.add("header-small");
-    else header.classList.remove("header-small");
-  });
-}
